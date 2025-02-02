d60bf4fe5aa07bab51103c8314d58ee450701acd
Gisele-123 <giselemigisha53@gmail.com>
2025-02-02 18:54:46 +0200

    code decoded

--- Decoding/codeDecoded.py
+++ Decoding/codeDecoded.py
@@ -1,5 +1,5 @@
 from aspose.barcode import barcoderecognition
 reader = barcoderecognition.BarCodeReader("image1.jpg", barcoderecognition.DecodeType.ALL_SUPPORTED_TYPES)
-recognized_results = reader.read_bar_codes()
-for barcode in recognized_results:
+results = reader.read_bar_codes()
+for barcode in results:
     print(barcode.code_text)
